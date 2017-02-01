3890c86574223525842aa48e865806c99b1e277b
Kai Li <Kai Li>
2017-02-01 00:40:27 -0500

    Implementation for Linkedlist data structure

---
+++ LinkedListLab/src/data_structures/StringArray.java
@@ -0,0 +1,153 @@
+package data_structures;
+
+import java.lang.IndexOutOfBoundsException;
+
+/**
+ * CSE 274
+ * StringArray: An implementation of a dynamic-sized string array.
+ * 
+ * @author : Kai Li
+ * 
+ * September 14, 2016
+ *
+ */
+
+public class StringArray extends Sequence<String>{
+
+	/**
+	 * A: Contains the actual array.
+	 */
+	private String[] A;
+
+
+	/**
+	 * Basic constructor: Creates an "empty" list.
+	 */
+	public StringArray() {
+		A = new String[1];
+		n = 0;
+	}
+
+	
+	/**
+	 * Get the string i.
+	 * 
+	 * @param i
+	 *            Number of accessed element (from 0).
+	 * @return string
+	 * @throws IndexOutOfBoundsException
+	 *             if index out of bounds
+	 */
+	public String get(int i) {
+		if (i < 0 || i >= size())
+			throw new IndexOutOfBoundsException();
+		numOps++;
+		return A[i];
+	}
+
+	/**
+	 * Set the string at element i to value.
+	 * 
+	 * @param i
+	 *            Number of accessed element (from 0).
+	 * @param value
+	 *            String value being put in the array
+	 * @return string
+	 * @throws IndexOutOfBoundsException
+	 *             if index out of bounds
+	 */
+	public String set(int i, String value) {
+		if (i < 0 || i >= size())
+			throw new IndexOutOfBoundsException();
+		String y = A[i];
+		numOps++;
+		A[i] = value;
+		numOps++;
+		return y;
+	}
+
+	/**
+	 * Insert a new value at index i (bumping elements i - size() up one).
+	 * 
+	 * @param i
+	 *            Number of inserted element
+	 * @param value
+	 *            String valueb eing inserted
+	 * @throws IndexOutOfBoundsException
+	 *             if index out of bounds
+	 */
+	public void add(int i, String value) {
+		if (i < 0 || i > size())
+			throw new IndexOutOfBoundsException();
+		
+		String oldArr[] = A; 
+		resize(); // This will make sure A is large enough to hold the new
+					// element
+		for (int j = A.length-1; j >i; j--) {
+			A[j] = oldArr[j-1]; // shifting elements by assigning 
+								// from oldArr[] to A
+			numOps++;
+		}
+		A[i] = value; 
+		numOps++;
+		n += 1; // change n (array size) after adding
+	}
+
+
+	/**
+	 * Remove element i from the array (renumbering everything past it)
+	 * 
+	 * @param i
+	 *            element being removed
+	 * @return value that was removed
+	 * @throws IndexOutOfBoundsException
+	 *             if index out of bounds
+	 */
+	public String remove(int i) {
+		if (i < 0 || i >= size())
+			throw new IndexOutOfBoundsException();
+		String value = A[i];
+		numOps++;
+		int shiftIndex = 0;
+		if (i != A.length - 1) { //if i is NOT the last element,
+							     //otherwise leave the value in there
+								 //assuming it has been "remove"
+			for (int j = i; j < A.length; j++) {
+				shiftIndex = j + 1;
+				if (shiftIndex < A.length) {
+					A[j] = A[shiftIndex];// shifting elements
+					numOps++;
+				}
+			}
+		}
+		n -= 1;
+		return value;
+	}
+
+
+	/**
+	 * If the array is full (that is: A.length == n), resize it so it can hold
+	 * one more element.
+	 */
+	private void resize() {
+		if (n < A.length) {
+			return; //return when n < A.length
+		}
+		//otherwise resizing A[] by adding one more space in there
+		String oldArr[] = A;
+		A = new String[n + 1];
+		for (int i = 0; i < oldArr.length; i++) {
+			A[i] = oldArr[i];
+			numOps++;
+		}
+	}
+	
+	
+	/**
+	 * Remove all elements.
+	 */
+	public void clear() {
+		A = new String[1];
+		n = 0;
+	}
+}
